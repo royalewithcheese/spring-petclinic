6f1d7aea8c5113a3e7b1c5a677348be6a73ab41f
royalewithcheese <royalewithcheese@users.noreply.github.com>
2018-01-04 13:44:40 -0600

    Update PetClinicApplication.java
    Add missing import statement

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -18,6 +18,7 @@ package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * PetClinic Spring Boot Application.
