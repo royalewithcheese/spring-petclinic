a6bb80cfdccfece7f1b63b38cb77d2e11f08ac58
royalewithcheese <royalewithcheese@users.noreply.github.com>
2018-01-04 13:37:18 -0600

    Update PetClinicApplication.java
    Changes in order to make the war deployable in WildFly

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -26,7 +26,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class PetClinicApplication {
+public class PetClinicApplication extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(PetClinicApplication.class, args);
